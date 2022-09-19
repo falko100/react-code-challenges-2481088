1bb251d0ac5f0d0d7f3c5e7e435279dcc9776eb1
Falko Woudstra <falko@coddin.nl>
2022-09-19 18:15:19 +0200

    wip

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,25 @@
-export default function DogPics () {
+import { useEffect, useState } from 'react';
+
+export default function DogPics() {
   // API: https://dog.ceo/dog-api/
+  const [dogImage, setDogImage] = useState(
+    '');
+
+  async function fetchNewDog() {
+    fetch('https://dog.ceo/api/breeds/image/random').then(async (response) => {
+      const data = await response.json();
+      setDogImage(data.message);
+    });
+  }
+
+  useEffect(() => {
+    fetchNewDog();
+  }, [])
+
   return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+    <div className="dog-pics">
+      {dogImage && <img src={dogImage} alt="Random Dog" height={400} />}
+      <button onClick={fetchNewDog}>🐶</button>
     </div>
-  )
+  );
 }
